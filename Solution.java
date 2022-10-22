a9b63a6de5aa02f95465938084a90029ff90b0f7
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-10-22 08:17:02 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,22 @@
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Solution {
+
+    public boolean uniqueOccurrences(int[] input) {
+        Map<Integer, Integer> valueToFrequency = new HashMap<>();
+        for (int n : input) {
+            valueToFrequency.put(n, valueToFrequency.getOrDefault(n, 0) + 1);
+        }
+        Set<Integer> uniqueFrequencies = new HashSet<>();
+        for (int n : valueToFrequency.values()) {
+            if (!uniqueFrequencies.add(n)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
