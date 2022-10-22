0279afa09f7a1d1ee5ba508f0ee9160b09bf5897
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-10-22 08:18:09 +0200

    Update Solution.java

--- Solution.java
+++ Solution.java
@@ -11,6 +11,7 @@ public class Solution {
         for (int n : input) {
             valueToFrequency.put(n, valueToFrequency.getOrDefault(n, 0) + 1);
         }
+        
         Set<Integer> uniqueFrequencies = new HashSet<>();
         for (int n : valueToFrequency.values()) {
             if (!uniqueFrequencies.add(n)) {
