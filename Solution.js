6f8b4d934f821c618dabc275b31852d2524d4619
Lachezar Kutsarov <lkutsarov@gmail.com>
2022-10-22 08:17:29 +0200

    Create Solution.js

---
+++ Solution.js
@@ -0,0 +1,22 @@
+
+/**
+ * @param {number[]} input
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (input) {
+    const valueToFrequency = new Map();
+    for (let n of input) {
+        if (!valueToFrequency.has(n)) {
+            valueToFrequency.set(n, 0);
+        }
+        valueToFrequency.set(n, valueToFrequency.get(n) + 1);
+    }
+    const uniqueFrequencies = new Set();
+    for (let n of valueToFrequency.values()) {
+        if (uniqueFrequencies.has(n)) {
+            return false;
+        }
+        uniqueFrequencies.add(n);
+    }
+    return true;
+};
